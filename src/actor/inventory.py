61d5f9bf3088446197244d91698e4e4e664fd718
Cody Poll <cjpoll@gmail.com>
2016-09-25 20:10:28 -0600

    First stab at character inventory.

--- src/actor/inventory.py
+++ src/actor/inventory.py
@@ -1,9 +1,24 @@
-class ActorInventory():
-	def __init__(self):
-		self.items = []
+class InventoryFullException(Exception):
+	pass
 
-	def add_item(self, item):
+class ActorInventory():
+	def __init__(self, max_size):
+		self.items = []
+		self.max_size = max_size
+
+	def __iter__(self):
+		return iter(self.items)
+
+	def add(self, item):
+		if self.length() >= self.max_size:
+			raise InventoryFullException
 		self.items.append(item)
+
+	def contains(self, item):
+		return item in self.items
 
 	def length(self):
 		return len(self.items)
+
+	def remove(self, item):
+		self.items.remove(item)
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,15 +1,49 @@
 import unittest
 
 from src.actor.inventory import ActorInventory
+from src.actor.inventory import InventoryFullException
 
+class TestItem():
+	def __init__(self, name):
+		self.name = name
 
 class TestActorInventory(unittest.TestCase):
 	def setUp(self):
-		self.inventory = ActorInventory()
-		self.item = object()
-	
+		self.inventory = ActorInventory(3)
+
 	def test_length_on_add(self):
-		self.inventory.add_item(self.item)
-		self.inventory.add_item(self.item)
-		self.inventory.add_item(self.item)
-		self.assertEqual(self.inventory.length(), 3)
+		self.inventory.add(TestItem("Mighty Axe"))
+		self.assertEqual(self.inventory.length(), 1)
+
+	def test_length_on_remove(self):
+		axe = TestItem("Mighty Axe")
+		self.inventory.add(axe)
+		self.inventory.add(TestItem("Magic Mirror"))
+		self.inventory.remove(axe)
+		self.assertEqual(self.inventory.length(), 1)
+
+	def test_containse_on_add(self):
+		axe = TestItem("Mighty Axe")
+		self.assertFalse(self.inventory.contains(axe))
+		self.inventory.add(axe)
+		self.assertTrue(self.inventory.contains(axe))
+
+	def test_contains_on_remove(self):
+		axe = TestItem("Mighty Axe")
+		self.inventory.add(axe)
+		self.inventory.remove(axe)
+		self.assertFalse(self.inventory.contains(axe))
+
+	def test_iterable(self):
+		self.inventory.add(1)
+		self.inventory.add(2)
+		doubles = map(lambda x: x * 2, self.inventory)
+		self.assertEqual(doubles, [2, 4])
+
+	def test_max_size(self):
+		axe = TestItem("Mighty Axe")
+		self.inventory.add(axe)
+		self.inventory.add(axe)
+		self.inventory.add(axe)
+		with self.assertRaises(InventoryFullException):
+			self.inventory.add(axe)
