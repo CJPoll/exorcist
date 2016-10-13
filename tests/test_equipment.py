63c6b96067f6a88d720b9c86a2a59ab064d534c0
Jakobhartman <Jakobhartman@hotmail.com>
2016-10-13 18:15:17 -0500

    finished test case for summing equipment need an actor object

--- tests/test_equipment.py
+++ tests/test_equipment.py
@@ -13,23 +13,29 @@ class TestEquipment(unittest.TestCase):
 		self.armor = Equipment("Leather Armor","ltarmor.png",1,0,1,EquipmentType.ARMOR)
 		self.nothing = EquipmentManager().NOTHING
 		self.actor
+		self.total = {1,1,1}
 	}
 	
 	def test_equipment_equip_weapon(self):
 		self.actor = EquipmentManager().equip(self.actor,self.weapon)
 		self.weapon.assertEquals(self.actor.weapon)
 
-	def test_equipment_equip_armor():
+	def test_equipment_equip_armor(self):
 		self.actor = EquipmentManager().equip(self.actor,self.armor)
 		self.armor.assertEquals(self.actor.armor)
 
 
-	def test_equipment_dequip_weapon(actor):
+	def test_equipment_dequip_weapon(self):
 		self.actor = EquipmentManager().dequip(self.actor,self.weapon)
 		self.nothing.assertEquals(self.actor.weapon)
 
-	def test_equipment_dequip_weapon(actor):
+	def test_equipment_dequip_weapon(self):
 		self.actor = EquipmentManager().dequip(self.actor,self.weapon)
 		self.nothing.assertEquals(self.actor.armor)
+
+	def test_equipment_sumEquipment():
+		self.total.assertEquals(EquipmentManager().sumEquipment(self.actor))
+
+
 
 
